c61ecb2f61b8561c9948957e21435c978743c3c3
Xudong Han <xudongh1@student.unimelb.edu.au>
2021-01-21 02:06:07 +1100

    Update deepmoji_sa.py

--- networks/deepmoji_sa.py
+++ networks/deepmoji_sa.py
@@ -18,6 +18,8 @@ class DeepMojiModel(nn.Module):
         try:
             if args.AF == "relu":
                 self.AF = self.ReLU
+            elif args.AF == "tanh":
+                self.AF = self.tanh
         except:
             pass
         self.dense1 = nn.Linear(self.emb_size, self.hidden_size)
@@ -45,124 +47,4 @@ class DeepMojiModel(nn.Module):
                 return out
             else:
                 out = self.dense3(out)
-                return out
-
-
-
-class FEDA_classifier(nn.Module):
-    def __init__(self, args):
-        super(FEDA_classifier, self).__init__()
-        self.args = args
-        self.emb_size = self.args.emb_size
-        self.hidden_size = args.hidden_size
-        self.num_classes = args.num_classes
-        self.LeakyReLU = nn.LeakyReLU()
-
-        # adv level
-        self.adv_level = self.args.adv_level
-
-        # first fully connected layer
-        self.fc1_s = nn.Linear(self.emb_size, self.hidden_size)
-        self.fc1_p1 = nn.Linear(self.emb_size, self.hidden_size)
-        self.fc1_p2 = nn.Linear(self.emb_size, self.hidden_size)
-        # second fully connected layer
-        self.fc2_s = nn.Linear(self.hidden_size, self.hidden_size)
-        self.fc2_p1 = nn.Linear(self.hidden_size, self.hidden_size)
-        self.fc2_p2 = nn.Linear(self.hidden_size, self.hidden_size)
-        # output layer
-        self.fc3 = nn.Linear(self.hidden_size, self.num_classes) 
-
-    def forward(self, input, private_info):
-        if self.args.original_FEDA:
-            # Performaing original FEDA
-            # Process each channel saperately and add outputs together before the last layer. 
-            # Shared 
-            out_s = self.fc1_s(input)
-            out_s = self.LeakyReLU(out_s)                        
-            out_s = self.fc2_s(out_s)
-
-            # Private 1
-            out_p1 = self.fc1_p1(input)
-            out_p1 = self.LeakyReLU(out_p1)
-            out_p1 = self.fc2_p1(out_p1)
-
-            # Private 2 
-            out_p2 = self.fc1_p2(input)
-            out_p2 = self.LeakyReLU(out_p2)
-            out_p2 = self.fc2_p2(out_p2)
-
-            # selecting channels according to private info
-            # removing a certain channel by times 0 
-            out_p1 = out_p1 * (private_info.float().reshape(-1,1))
-            out_p2 = out_p2 * (((private_info-1)**2).float().reshape(-1,1))
-            # Merging
-            out = torch.add(torch.add(out_s, out_p1), out_p2) # Batch_size * hidden_size
-
-            # Output
-            out = self.fc3(out)
-            return out
-        else:
-            # FEDA-Performing correction at each layer
-            # Use private information at each layer
-
-            # First layer
-            out_s = self.fc1_s(input)
-            out_p1 = self.fc1_p1(input)
-            out_p2 = self.fc1_p2(input)
-            # correction
-            out_p1 = out_p1 * (private_info.float().reshape(-1,1))
-            out_p2 = out_p2 * (((private_info-1)**2).float().reshape(-1,1))
-            # adding
-            corrected_out_1 = torch.add(torch.add(out_s, out_p1), out_p2)
-            
-            # Activation function
-            corrected_out_1 = self.LeakyReLU(corrected_out_1)
-
-            # Second layer
-            out_s = self.fc2_s(corrected_out_1)
-            out_p1 = self.fc2_p1(corrected_out_1)
-            out_p2 = self.fc2_p2(corrected_out_1)
-            # correction
-            out_p1 = out_p1 * (private_info.float().reshape(-1,1))
-            out_p2 = out_p2 * (((private_info-1)**2).float().reshape(-1,1))
-            # adding
-            corrected_out_2 = torch.add(torch.add(out_s, out_p1), out_p2)
-            
-            # Output
-            out = self.fc3(corrected_out_2)
-            return out
-
-    def shared_pred(self, input):
-        """
-        Input: vectors
-        Output:
-            predictions
-            shared representations
-        """
-        out_s = self.fc1_s(input)
-        out_s = self.LeakyReLU(out_s)                        
-        out_s = self.fc2_s(out_s) 
-        # Output
-        out = self.fc3(out)
-        return out
-
-    def hidden(self, input):
-        """
-        Input: vectors
-        Output:
-            predictions
-            shared representations
-        """
-        assert self.adv_level in set([0, -1, -2])
-        out_s = self.fc1_s(input)
-        if self.adv_level == -2:
-            return out_s
-        else:
-            out_s = self.LeakyReLU(out_s)
-            out_s = self.fc2_s(out_s)
-            if self.adv_level == -1:
-                return out_s
-            else:
-                # Output
-                out = self.fc3(out)
                 return out
